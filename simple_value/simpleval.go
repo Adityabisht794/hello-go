6973470610ebbfc3606f46dadc717e37867d8c1b
Adityabisht794 <adityabisht220111794@gmail.com>
2026-06-23 11:24:46 +0000

    Simple values

---
+++ simple_value/simpleval.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fname string = "Adiytaa"
+	fmt.Println(fname)
+	var lname = "Bisht"
+	fmt.Println(lname)
+	var age int = 22
+	fmt.Println(age)
+	var isMarried bool = false
+	fmt.Println(isMarried)
+	var height float64 = 5.8
+	fmt.Println(height)
+	var area = 3.14 * height * height
+	fmt.Println(area)
+}
